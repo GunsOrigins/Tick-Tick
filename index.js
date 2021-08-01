3e403a07ddf3eba7a0ee6641a57b05d8aac60162
GunsOrigins <gunnchayut@gmail.com>
2021-08-01 15:55:37 +0700

    build JS file

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 //put timer things here
-class Timer {
-    minutesCount : number;
-    secondsCount : number;
-    toggleState : boolean;
-    constructor() {
+var Timer = /** @class */ (function () {
+    function Timer() {
         this.minutesCount = 0;
         this.secondsCount = 0;
         this.toggleState = false;
     }
-}
-
-const tmr = new Timer();
-
-
+    return Timer;
+}());
+var tmr = new Timer();
 /** Clear the timer input */
 function clearInput() {
     tmr.minutesCount = 0;
@@ -26,7 +21,7 @@ function AlarmUser() {
 //add interactivity to timerToggle
 /** Start/stop the timer */
 function startstopTimer() {
-    let processedTime = tmr.minutesCount * 60 + tmr.secondsCount;
+    var processedTime = tmr.minutesCount * 60 + tmr.secondsCount;
     if (processedTime != 0) {
         ToggleTimer(true, processedTime);
     }
@@ -38,18 +33,18 @@ function startstopTimer() {
  * Toggle the timer
  * @param {boolean} state - The state you wanna toggle! true = start the timer, false = pause the timer
  * @param {number=} data - The data you wanna pass with
- * 
+ *
  */
-function ToggleTimer(state: boolean, data?: number) {
-        if (state === false) {
-            tmr.toggleState = false;
-            displayToggle();
-        }
-        else if (state === true) {
-            tmr.toggleState = true;
-            displayToggle();
-            return console.log(data);
-        }
+function ToggleTimer(state, data) {
+    if (state === false) {
+        tmr.toggleState = false;
+        displayToggle();
+    }
+    else if (state === true) {
+        tmr.toggleState = true;
+        displayToggle();
+        return console.log(data);
+    }
 }
 //extract to a function because yes
 function displayToggle() {
